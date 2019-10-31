417486198ff5aa847c02b7bdce140347c6d2714d
Cory Reed <swashcap@gmail.com>
2019-10-31 01:35:46 -0700

    feat: add global header, nav

---
+++ components/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export type NavProps = React.HTMLProps<HTMLDivElement>
+
+export const Nav: React.FC<NavProps> = props => (
+  <nav {...props}>
+    <ul className="flex list ma0 nested-links pa0">
+      <li>
+        <a className="dib no-underline pa3" href="#" rel="bookmark">Link 1</a>
+      </li>
+      <li>
+        <a className="dib no-underline pa3" href="#" rel="bookmark">Link 2</a>
+      </li>
+    </ul>
+  </nav>
+)
--- components/Root.tsx
+++ components/Root.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import clsx from 'clsx'
 import '../node_modules/tachyons/css/tachyons.css'
+import { Nav } from './Nav'
 
 export interface RootProps extends React.HTMLAttributes<HTMLDivElement> {
   description?: string
@@ -20,6 +21,10 @@ export const Root: React.FC<RootProps> = ({
       <title>{title}</title>
       {!!description && <meta name="description" content={description} />}
     </Head>
-    {children}
+    <header className="bb b--silver items-center flex" role="banner">
+      <h1 className="f5 fw4 mv0 pr3 pv3">Hello, Next.js!</h1>
+      <Nav />
+    </header>
+    <main>{children}</main>
   </div>
 )
