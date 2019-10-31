629c454a79a50be119106f01476a7e50a25d5e20
Cory Reed <swashcap@gmail.com>
2019-10-31 00:33:03 -0700

    refactor: migrate to TypeScript

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,10 +4,7 @@ import Link from 'next/link'
 const links = [
   { href: 'https://zeit.co/now', label: 'ZEIT' },
   { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+]
 
 const Nav = () => (
   <nav>
@@ -17,8 +14,8 @@ const Nav = () => (
           <a>Home</a>
         </Link>
       </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
+      {links.map(({ href, label }) => (
+        <li key={`nav-link-${href}-${label}`}>
           <a href={href}>{label}</a>
         </li>
       ))}
